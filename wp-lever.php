637f6e6cb9f9dbcf647a16540c5ca3ca4a4c77f4
netsi <netsi@users.noreply.github.com>
2019-12-06 11:39:35 +0100

    fix syntax error

--- wp-lever.php
+++ wp-lever.php
@@ -47,7 +47,7 @@ if ( ! class_exists( 'WP_Lever' ) ) {
 	 * Class WP_Lever
 	 */
 	class WP_Lever {
-		const VERSION = "1.0.7";
+		const VERSION = "1.0.8";
 
 		/**
 		 * Slug used in various places.
@@ -89,7 +89,7 @@ if ( ! class_exists( 'WP_Lever' ) ) {
 				"main",
 				plugin_dir_url( __FILE__ ) . 'css/main.css',
 				null,
-				self::VERSION,
+				self::VERSION
 			);
 
 			wp_enqueue_script(
